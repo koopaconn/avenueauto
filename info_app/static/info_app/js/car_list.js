5402b5d107fec169ab674cd3eae6d3a0550f6ed2
mconn <mconn@mriglobal.org>
2018-09-21 15:09:56 -0500

    First draft

--- info_app/static/info_app/js/car_list.js
+++ info_app/static/info_app/js/car_list.js
@@ -1,1 +1,7 @@
-$('img').tooltip('update')
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  $('a').tooltip('show')
+}
+
+else {
+  $('img').tooltip('update')
+}
